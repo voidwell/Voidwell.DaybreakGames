58341270d66c29620a950b06434039b5b993381a
Jess Tremper <jess@voidwell.com>
2017-12-18 20:37:22 -0500

    Add character data details query and processing.

--- src/Voidwell.DaybreakGames/Models/CharacterDetails.cs
+++ src/Voidwell.DaybreakGames/Models/CharacterDetails.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Voidwell.DaybreakGames.Models
 {
@@ -10,19 +11,145 @@ namespace Voidwell.DaybreakGames.Models
         public int BattleRankPercentToNext { get; set; }
         public int CertsEarned { get; set; }
         public string Faction { get; set; }
-        public int FactionId { get; set; }
-        public int FactionImageId { get; set; }
+        public string FactionId { get; set; }
+        public string FactionImageId { get; set; }
         public string Title { get; set; }
-        public int WorldId { get; set; }
+        public string World { get; set; }
+        public string WorldId { get; set; }
 
-        /*
-        public CharacterLifetimeStats LifetimeStats { get; set; }
-        public CharacterOutfit Outfit { get; set; }
-        public IEnumerable<CharacterStat> ProfileStats { get; set; }
-        public IEnumerable<CharacterStatByFaction> ProfileStatsByFaction { get; set; }
-        public CharacterTimes Times { get; set; }
-        public IEnumerable<CharacterVehicleStats> VehicleStats { get; set; }
-        public IEnumerable<CharacterWeaponStats> WeaponStats { get; set; }
-        */
+        public CharacterDetailsTimes Times { get; set; }
+        public CharacterDetailsOutfit Outfit { get; set; }
+        public CharacterDetailsLifetimeStats LifetimeStats { get; set; }
+        public IEnumerable<CharacterDetailsProfileStat> ProfileStats { get; set; }
+        public IEnumerable<CharacterDetailsProfileStatByFaction> ProfileStatsByFaction { get; set; }
+        public IEnumerable<CharacterDetailsWeaponStat> WeaponStats { get; set; }
+        public IEnumerable<CharacterDetailsVehicleStat> VehicleStats { get; set; }
     }
+
+    public class CharacterDetailsTimes
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastSaveDate { get; set; }
+        public DateTime LastLoginDate { get; set; }
+        public int MinutesPlayed { get; set; }
+    }
+
+    public class CharacterDetailsOutfit
+    {
+        public string Id { get; set; }
+        public DateTime MemberSinceDate { get; set; }
+        public string Rank { get; set; }
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int MemberCount { get; set; }
+    }
+
+    public class CharacterDetailsLifetimeStats
+    {
+        public int AchievementCount { get; set; }
+        public int AssistCount { get; set; }
+        public int FacilityDefendedCount { get; set; }
+        public int MedalCount { get; set; }
+        public int SkillPoints { get; set; }
+        public int Deaths { get; set; }
+        public int FireCount { get; set; }
+        public int HitCount { get; set; }
+        public int PlayTime { get; set; }
+        public int Score { get; set; }
+        public int DominationCount { get; set; }
+        public int FacilityCaptureCount { get; set; }
+        public int RevengeCount { get; set; }
+        public int DamageGiven { get; set; }
+        public int DamageTakenBy { get; set; }
+        public int Headshots { get; set; }
+        public int KilledBy { get; set; }
+        public int Kills { get; set; }
+        public int VehicleKills { get; set; }
+    }
+
+    public class CharacterDetailsProfileStat
+    {
+        public string ProfileId { get; set; }
+        public string ProfileName { get; set;}
+        public string ImageId { get; set; }
+        public int Deaths { get; set; }
+        public int FireCount { get; set; }
+        public int HitCount { get; set; }
+        public int PlayTime { get; set; }
+        public int Score { get; set; }
+        public int KilledBy { get; set; }
+        public int Kills { get; set; }
+    }
+
+    public class CharacterDetailsProfileStatByFaction
+    {
+        public string ProfileId { get; set; }
+        public string ProfileName { get; set; }
+        public string ImageId { get; set; }
+        public CharacterDetailsProfileStatByFactionValue KilledBy { get; set; }
+        public CharacterDetailsProfileStatByFactionValue Kills { get; set; }
+    }
+
+    public class CharacterDetailsProfileStatByFactionValue
+    {
+        public int Vs { get; set; }
+        public int Nc { get; set; }
+        public int Tr { get; set; }
+    }
+
+    public class CharacterDetailsWeaponStat
+    {
+        public string ItemId { get; set; }
+        public string Category { get; set; }
+        public string ImageId { get; set; }
+        public string Name { get; set; }
+        public string VehicleId { get; set; }
+        public string VehicleName { get; set; }
+        public string VehicleImageId { get; set; }
+        public CharacterDetailsWeaponStatValue Stats { get; set; }
+    }
+
+    public class CharacterDetailsWeaponStatValue
+    {
+        public int? Deaths { get; set; }
+        public int? FireCount { get; set; }
+        public int? HitCount { get; set; }
+        public int? PlayTime { get; set; }
+        public int? DamageGiven { get; set; }
+        public int? Headshots { get; set; }
+        public int? Kills { get; set; }
+        public int? VehicleKills { get; set; }
+        public int? Score { get; set; }
+        public int? DamageTakenBy { get; set; }
+        public int? KilledBy { get; set; }
+        public double? KillDeathRatio { get; set; }
+        public double? Accuracy { get; set; }
+        public double? HeadshotRatio { get; set; }
+        public double? KillsPerHour { get; set; }
+        public double? VehicleKillsPerHour { get; set; }
+        public double? ScorePerMinute { get; set; }
+        public double? LandedPerKill { get; set; }
+        public double? ShotsPerKill { get; set; }
+    }
+
+    public class CharacterDetailsVehicleStat
+    {
+        public string VehicleId { get; set; }
+        public int DamageTakenBy { get; set; }
+        public int KilledBy { get; set; }
+        public int Score { get; set; }
+        public int Deaths { get; set; }
+        public int FireCount { get; set; }
+        public int HitCount { get; set; }
+        public int PlayTime { get; set; }
+        public int DamageGiven { get; set; }
+        public int Headshots { get; set; }
+        public int Kills { get; set; }
+        public int VehicleKills { get; set; }
+    }
+
+
+
+
 }
--- src/Voidwell.DaybreakGames/Services/Planetside/CharacterService.cs
+++ src/Voidwell.DaybreakGames/Services/Planetside/CharacterService.cs
@@ -8,7 +8,6 @@ using Voidwell.DaybreakGames.Data.Models.Planetside;
 using Voidwell.Cache;
 using Voidwell.DaybreakGames.Data.Repositories;
 using Microsoft.Extensions.Logging;
-using Voidwell.DaybreakGames.CensusServices.Models;
 
 namespace Voidwell.DaybreakGames.Services.Planetside
 {
@@ -52,18 +51,147 @@ namespace Voidwell.DaybreakGames.Services.Planetside
             return character ?? await UpdateCharacter(characterId);
         }
 
-        public async Task<DbCharacter> GetCharacterDetails(string characterId)
+        public async Task<CharacterDetails> GetCharacterDetails(string characterId)
         {
-            var character = await TryGetCharacterFull(characterId);
+            var character = await GetCharacterDetailsAsync(characterId);
 
-            if (character != null && character.Time != null)
+            var lifetimeStats = character.Stats.FirstOrDefault(a => a.ProfileId == "0");
+
+            var details = new CharacterDetails
             {
-                return character;
-            }
+                Id = character.Id,
+                Name = character.Name,
+                FactionId = character.FactionId,
+                Faction = character.Faction.Name,
+                FactionImageId = character.Faction.ImageId,
+                BattleRank = character.BattleRank,
+                BattleRankPercentToNext = character.BattleRankPercentToNext,
+                CertsEarned = character.CertsEarned,
+                Title = character.Title.Name,
+                WorldId = character.WorldId,
+                World = character.World.Name,
+                Outfit = new CharacterDetailsOutfit
+                {
+                    Id = character.OutfitMembership.Outfit.Id,
+                    Name = character.OutfitMembership.Outfit.Name,
+                    Alias = character.OutfitMembership.Outfit.Alias,
+                    CreatedDate = character.OutfitMembership.Outfit.CreatedDate,
+                    MemberCount = character.OutfitMembership.Outfit.MemberCount,
+                    MemberSinceDate = character.OutfitMembership.MemberSinceDate,
+                    Rank = character.OutfitMembership.Rank
+                },
+                Times = new CharacterDetailsTimes
+                {
+                    CreatedDate = character.Time.CreatedDate,
+                    LastLoginDate = character.Time.LastLoginDate,
+                    LastSaveDate = character.Time.LastSaveDate,
+                    MinutesPlayed = character.Time.MinutesPlayed
+                },
+                LifetimeStats = new CharacterDetailsLifetimeStats
+                {
+                    AchievementCount = lifetimeStats.AchievementCount.Value,
+                    AssistCount = lifetimeStats.AssistCount.Value,
+                    DamageGiven = lifetimeStats.WeaponDamageGiven.Value,
+                    DamageTakenBy = lifetimeStats.WeaponDamageTakenBy.Value,
+                    Deaths = lifetimeStats.Deaths.Value,
+                    DominationCount = lifetimeStats.DominationCount.Value,
+                    FacilityCaptureCount = lifetimeStats.FacilityCaptureCount.Value,
+                    FacilityDefendedCount = lifetimeStats.FacilityDefendedCount.Value,
+                    FireCount = lifetimeStats.FireCount.Value,
+                    HitCount = lifetimeStats.HitCount.Value,
+                    Headshots = lifetimeStats.WeaponHeadshots.Value,
+                    KilledBy = lifetimeStats.KilledBy.Value,
+                    Kills = lifetimeStats.Kills.Value,
+                    MedalCount = lifetimeStats.MedalCount.Value,
+                    PlayTime = lifetimeStats.PlayTime.Value,
+                    RevengeCount = lifetimeStats.RevengeCount.Value,
+                    Score = lifetimeStats.Score.Value,
+                    SkillPoints = lifetimeStats.Kills.Value,
+                    VehicleKills = lifetimeStats.WeaponVehicleKills.Value
+                },
+                ProfileStats = character.Stats.Select(s => new CharacterDetailsProfileStat
+                {
+                    ProfileId = s.ProfileId,
+                    ProfileName = s.Profile?.Name,
+                    ImageId = s.Profile?.ImageId,
+                    Deaths = s.Deaths.Value,
+                    FireCount = s.FireCount.Value,
+                    HitCount = s.HitCount.Value,
+                    KilledBy = s.KilledBy.Value,
+                    Kills = s.Kills.Value,
+                    PlayTime = s.PlayTime.Value,
+                    Score = s.Score.Value
+                }),
+                ProfileStatsByFaction = character.StatsByFaction.Select(s => new CharacterDetailsProfileStatByFaction
+                {
+                    ProfileId = s.ProfileId,
+                    ProfileName = s.Profile?.Name,
+                    ImageId = s.Profile?.ImageId,
+                    Kills = new CharacterDetailsProfileStatByFactionValue
+                    {
+                        Vs = s.KillsVS.Value,
+                        Nc = s.KillsNC.Value,
+                        Tr = s.KillsTR.Value
+                    },
+                    KilledBy = new CharacterDetailsProfileStatByFactionValue
+                    {
+                        Vs = s.KilledByVS.Value,
+                        Nc = s.KilledByNC.Value,
+                        Tr = s.KilledByTR.Value
+                    }
+                }),
+                WeaponStats = character.WeaponStats.Select(s => new CharacterDetailsWeaponStat
+                {
+                    ItemId = s.ItemId,
+                    Name = s.Item?.Name,
+                    Category = s.Item?.ItemCategoryId,
+                    ImageId = s.Item?.ImageId,
+                    VehicleId = s.VehicleId,
+                    VehicleName = s.Vehicle?.Name,
+                    VehicleImageId = s.Vehicle?.ImageId,
+                    Stats = new CharacterDetailsWeaponStatValue
+                    {
+                        DamageGiven = s.DamageGiven,
+                        DamageTakenBy = s.DamageTakenBy,
+                        Kills = s.Kills,
+                        Deaths = s.Deaths,
+                        FireCount = s.FireCount,
+                        HitCount = s.HitCount,
+                        Headshots = s.Headshots,
+                        KilledBy = s.KilledBy,
+                        PlayTime = s.PlayTime,
+                        Score = s.Score,
+                        VehicleKills = s.VehicleKills,
+                        /*
+                        Accuracy = s.HitCount.Value > 0 ? s.FireCount.Value / s.HitCount.Value : 0,
+                        HeadshotRatio = s.Kills.Value > 0 ? (s.Headshots.Value / s.Kills.Value) * 100 : 0,
+                        KillDeathRatio = s.Deaths.Value > 0 ? s.Kills.Value / s.Deaths.Value : 0,
+                        KillsPerHour = s.PlayTime.Value > 0 ? s.Kills.Value / (s.PlayTime.Value / 3600) : 0,
+                        LandedPerKill = s.Kills.Value > 0 ? s.HitCount.Value / s.Kills.Value : 0,
+                        ShotsPerKill = s.Kills.Value > 0 ? s.FireCount.Value / s.Kills.Value : 0,
+                        ScorePerMinute = s.PlayTime.Value > 0 ? s.Score.Value / (s.PlayTime.Value / 60) : 0,
+                        VehicleKillsPerHour = s.Kills.Value > 0 ? s.VehicleKills.Value / (s.Kills.Value / 3600) : 0
+                        */
+                    }
+                }),
+                VehicleStats = character.WeaponStats.GroupBy(a => a.VehicleId).Where(a => a.Key != null || a.Key != "0").Select(s => new CharacterDetailsVehicleStat
+                {
+                    VehicleId = s.Key,
+                    DamageGiven = s.Sum(a => a.DamageGiven.Value),
+                    DamageTakenBy = s.Sum(a => a.DamageTakenBy.Value),
+                    Deaths = s.Sum(a => a.Deaths.Value),
+                    FireCount = s.Sum(a => a.FireCount.Value),
+                    VehicleKills = s.Sum(a => a.VehicleKills.Value),
+                    Headshots = s.Sum(a => a.Headshots.Value),
+                    HitCount = s.Sum(a => a.HitCount.Value),
+                    KilledBy = s.Sum(a => a.KilledBy.Value),
+                    Kills = s.Sum(a => a.Kills.Value),
+                    PlayTime = s.Sum(a => a.PlayTime.Value),
+                    Score = s.Sum(a => a.Score.Value)
+                })
+            };
 
-            await UpdateCharacter(characterId, null);
-
-            return await TryGetCharacterFull(characterId);
+            return details;
         }
 
         public async Task UpdateCharacter(string characterId, DateTime? lastLoginDate = null)
@@ -85,9 +213,62 @@ namespace Voidwell.DaybreakGames.Services.Planetside
             return _outfitService.UpdateCharacterOutfitMembership(characterId);
         }
 
-        private Task<DbCharacter> TryGetCharacterFull(string characterId)
+        public Task<IEnumerable<DbPlayerSession>> GetSessions(string characterId)
         {
-            return _characterRepository.GetCharacterWithDetailsAsync(characterId);
+            return _playerSessionRepository.GetPlayerSessionsByCharacterIdAsync(characterId, 25);
+        }
+
+        public async Task<PlayerSession> GetSession(string characterId, string sessionId)
+        {
+            var session = await _playerSessionRepository.GetPlayerSessionAsync(sessionId);
+            if (session == null)
+            {
+                return null;
+            }
+
+            var sessionDeaths = await _eventRepository.GetDeathEventsForCharacterIdByDateAsync(characterId, session.LoginDate, session.LogoutDate);
+
+            var sessionEvents = sessionDeaths.Select(d => new PlayerSessionEvent
+            {
+                Attacker = new CombatReportItemDetail { Id = d.AttackerCharacterId, Name = d.AttackerCharacter.Name, FactionId = d.AttackerCharacter.FactionId },
+                Victim = new CombatReportItemDetail { Id = d.CharacterId, Name = d.Character.Name, FactionId = d.Character.FactionId },
+                Weapon = new PlayerSessionWeapon { Id = d.AttackerWeaponId, ImageId = d.AttackerWeapon.ImageId, Name = d.AttackerWeapon.Name },
+                Timestamp = d.Timestamp,
+                ZoneId = d.ZoneId,
+                IsHeadshot = d.IsHeadshot,
+                AttackerFireModeId = d.AttackerFireModeId,
+                AttackerLoadoutId = d.AttackerLoadoutId,
+                AttackerOutfitId = d.AttackerOutfitId,
+                AttackerVehicleId = d.AttackerVehicleId,
+                CharacterLoadoutId = d.CharacterLoadoutId,
+                CharacterOutfitId = d.CharacterOutfitId
+            });
+
+            return new PlayerSession
+            {
+                Events = sessionEvents,
+                Session = new PlayerSessionInfo
+                {
+                    CharacterId = session.CharacterId,
+                    Id = session.Id,
+                    Duration = session.Duration,
+                    LoginDate = session.LoginDate,
+                    LogoutDate = session.LogoutDate
+                }
+            };
+        }
+
+        public async Task<DbCharacter> GetCharacterDetailsAsync(string characterId)
+        {
+            var character = await _characterRepository.GetCharacterWithDetailsAsync(characterId);
+            if (character != null && character.Time != null)
+            {
+                return character;
+            }
+
+            await UpdateCharacter(characterId, null);
+
+            return await _characterRepository.GetCharacterWithDetailsAsync(characterId);
         }
 
         private async Task<DbCharacter> UpdateCharacter(string characterId)
@@ -168,7 +349,7 @@ namespace Voidwell.DaybreakGames.Services.Planetside
                     ProfileId = group.Key
                 };
 
-                foreach(var stat in group)
+                foreach (var stat in group)
                 {
                     StatValueMapper.AssignStatValue(ref dbModel, stat.StatName, stat.ValueForever);
                 }
@@ -217,7 +398,7 @@ namespace Voidwell.DaybreakGames.Services.Planetside
 
             var wepStats = characterWepStatsTask.Result;
             var wepStatsByFaction = characterWepStatsByFactionTask.Result;
-            
+
 
             if (wepStats == null && wepStatsByFaction == null)
             {
@@ -279,51 +460,6 @@ namespace Voidwell.DaybreakGames.Services.Planetside
 
             await _characterRepository.UpsertRangeAsync(statModels);
             await _characterRepository.UpsertRangeAsync(statByFactionModels);
-        }
-
-        public Task<IEnumerable<DbPlayerSession>> GetSessions(string characterId)
-        {
-            return _playerSessionRepository.GetPlayerSessionsByCharacterIdAsync(characterId, 25);
-        }
-
-        public async Task<PlayerSession> GetSession(string characterId, string sessionId)
-        {
-            var session = await _playerSessionRepository.GetPlayerSessionAsync(sessionId);
-            if (session == null)
-            {
-                return null;
-            }
-
-            var sessionDeaths = await _eventRepository.GetDeathEventsForCharacterIdByDateAsync(characterId, session.LoginDate, session.LogoutDate);
-
-            var sessionEvents = sessionDeaths.Select(d => new PlayerSessionEvent
-            {
-                Attacker = new CombatReportItemDetail { Id = d.AttackerCharacterId, Name = d.AttackerCharacter.Name, FactionId = d.AttackerCharacter.FactionId },
-                Victim = new CombatReportItemDetail { Id = d.CharacterId, Name = d.Character.Name, FactionId = d.Character.FactionId },
-                Weapon = new PlayerSessionWeapon { Id = d.AttackerWeaponId, ImageId = d.AttackerWeapon.ImageId, Name = d.AttackerWeapon.Name },
-                Timestamp = d.Timestamp,
-                ZoneId = d.ZoneId,
-                IsHeadshot = d.IsHeadshot,
-                AttackerFireModeId = d.AttackerFireModeId,
-                AttackerLoadoutId = d.AttackerLoadoutId,
-                AttackerOutfitId = d.AttackerOutfitId,
-                AttackerVehicleId = d.AttackerVehicleId,
-                CharacterLoadoutId = d.CharacterLoadoutId,
-                CharacterOutfitId = d.CharacterOutfitId
-            });
-
-            return new PlayerSession
-            {
-                Events = sessionEvents,
-                Session = new PlayerSessionInfo
-                {
-                    CharacterId = session.CharacterId,
-                    Id = session.Id,
-                    Duration = session.Duration,
-                    LoginDate = session.LoginDate,
-                    LogoutDate = session.LogoutDate
-                }
-            };
         }
     }
 }
