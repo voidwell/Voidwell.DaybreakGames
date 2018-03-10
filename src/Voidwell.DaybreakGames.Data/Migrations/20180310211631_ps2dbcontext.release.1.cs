eb17c2eff237e9fab0a925f27e9181bd5ea4dff2
Jess Tremper <jess@voidwell.com>
2018-03-10 16:23:07 -0500

    Update migrations

---
+++ src/Voidwell.DaybreakGames.Data/Migrations/20180310211631_ps2dbcontext.release.1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Voidwell.DaybreakGames.Data.Migrations
+{
+    public partial class ps2dbcontextrelease1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "p_k_event_facility_control",
+                table: "event_facility_control");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "new_faction_id",
+                table: "event_facility_control",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "p_k_event_facility_control",
+                table: "event_facility_control",
+                columns: new[] { "timestamp", "world_id", "facility_id", "new_faction_id" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "p_k_event_facility_control",
+                table: "event_facility_control");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "new_faction_id",
+                table: "event_facility_control",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "p_k_event_facility_control",
+                table: "event_facility_control",
+                columns: new[] { "timestamp", "world_id", "facility_id" });
+        }
+    }
+}
