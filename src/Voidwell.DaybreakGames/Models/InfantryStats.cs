3f486d6ed7b58e2915fdbf8a7ded71f3ac34b7dd
Lampjaw <matthewt89@gmail.com>
2018-06-03 19:13:07 -0400

    Add additional infantry stats

--- src/Voidwell.DaybreakGames/Models/InfantryStats.cs
+++ src/Voidwell.DaybreakGames/Models/InfantryStats.cs
@@ -8,9 +8,12 @@
         public double? Accuracy { get; set; }
         public double? HeadshotRatio { get; set; }
         public double? KillDeathRatio { get; set; }
+        public double? KillsPerMinute { get; set; }
         public double? KDRPadding { get; set; }
         public double? AccuracyDelta { get; set; }
         public double? HeadshotRatioDelta { get; set; }
+        public double? KillsPerMinuteDelta { get; set; }
+        public double? KillDeathRatioDelta { get; set; }
         public int? IVIScore { get; set; }
     }
 }
