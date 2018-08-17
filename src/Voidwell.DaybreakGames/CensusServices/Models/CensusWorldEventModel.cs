db7fe8124ac5def1d4ac43aad77cbccb6ca09b85
Jess Tremper <jess@voidwell.com>
2018-08-17 16:04:07 -0400

    Add census collector for world events

---
+++ src/Voidwell.DaybreakGames/CensusServices/Models/CensusWorldEventModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Voidwell.DaybreakGames.CensusServices.Models
+{
+    public class CensusMetagameWorldEventModel
+    {
+        public string EventType { get; set; }
+        public string TableType { get; set; }
+        public int Timestamp { get; set; }
+        public int ZoneId { get; set; }
+        public int WorldId { get; set; }
+
+        public int MetagameEventId { get; set; }
+        public int MetagameEventState { get; set; }
+        public int FactionNc { get; set; }
+        public int FactionTr { get; set; }
+        public int FactionVs { get; set; }
+        public int ExperienceBonus { get; set; }
+        public int InstanceId { get; set; }
+        public string MetagameEventStateName { get; set; }
+    }
+}
