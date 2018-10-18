449924f100707ba46115f0e0c6f34ed0699fc747
Jess Tremper <jess@voidwell.com>
2018-10-18 15:10:51 -0400

    Add endpoints to resolve zones and worlds

---
+++ src/Voidwell.DaybreakGames/Controllers/Planetside/WorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Voidwell.DaybreakGames.Services.Planetside;
+
+namespace Voidwell.DaybreakGames.Controllers.Planetside
+{
+    [Route("ps2/world")]
+    public class WorldController : Controller
+    {
+        private readonly IWorldService _worldService;
+
+        public WorldController(IWorldService worldService)
+        {
+            _worldService = worldService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllWorlds ()
+        {
+            var result = await _worldService.GetAllWorlds();
+            return Ok(result);
+        }
+    }
+}
