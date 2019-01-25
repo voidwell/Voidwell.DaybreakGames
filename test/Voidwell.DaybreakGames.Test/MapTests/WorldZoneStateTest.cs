60f8fdab1dce65e3aad82cc3ec03bfb2f450369a
Lampjaw <matthewt89@gmail.com>
2019-01-25 12:01:45 -0500

    Fix historical lock breaking map state tracking.

---
+++ test/Voidwell.DaybreakGames.Test/MapTests/WorldZoneStateTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Voidwell.DaybreakGames.Test.MapTests
+{
+    public class WorldZoneStateTest
+    {
+        [Theory]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        public void WorldZoneState_CalculateScore(int zoneId)
+        {
+            var worldZoneState = MapHelper.GetWorldZoneState(zoneId);
+
+            var score = worldZoneState.MapScore;
+
+            var percentTotal = score.Territories.Neutural.Percent + score.Territories.Vs.Percent + score.Territories.Nc.Percent + score.Territories.Tr.Percent;
+            var connectedPercentTotal = score.ConnectedTerritories.Neutural.Percent + score.ConnectedTerritories.Vs.Percent + score.ConnectedTerritories.Nc.Percent + score.ConnectedTerritories.Tr.Percent;
+
+            Assert.Equal(1, percentTotal);
+            Assert.Equal(1, connectedPercentTotal);
+        }
+    }
+}
