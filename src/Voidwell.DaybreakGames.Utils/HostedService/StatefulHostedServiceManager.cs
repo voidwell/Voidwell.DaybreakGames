7997071338497ca31a5efef883f121c478450569
Jess Tremper <jess@voidwell.com>
2023-03-01 12:16:44 -0500

    Update to dotnet 6

--- src/Voidwell.DaybreakGames.Utils/HostedService/StatefulHostedServiceManager.cs
+++ src/Voidwell.DaybreakGames.Utils/HostedService/StatefulHostedServiceManager.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Voidwell.Cache;
+using Voidwell.Microservice.Cache;
 
 namespace Voidwell.DaybreakGames.Utils.HostedService
 {
