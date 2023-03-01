034f30d6e0ec59a35d4460fb90162c73d4ea9087
Jess Tremper <jess@voidwell.com>
2023-03-01 12:18:02 -0500

    Add directive outlines to domain. Update to dotnet 6.

---
+++ src/Voidwell.DaybreakGames.Domain/Models/CharacterDirectivesOutline.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Voidwell.DaybreakGames.Domain.Models
+{
+    public class CharacterDirectivesOutline
+    {
+        public List<CharacterDirectivesOutlineTree> Trees { get; set; }
+    }
+
+    public class CharacterDirectivesOutlineTree
+    {
+        public int Id { get; set; }
+        public int CurrentDirectiveTierId { get; set; }
+        public int CurrentLevel { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+        public List<CharacterDirectivesOutlineTier> Tiers { get; set; }
+        public List<CharacterDirectivesOutlineDirective> Directives { get; set; }
+    }
+
+    public class CharacterDirectivesOutlineTier
+    {
+        public int TierId { get; set; }
+        public int TreeId { get; set; }
+        public DateTime? CompletionDate { get; set; }
+    }
+
+    public class CharacterDirectivesOutlineDirective
+    {
+        public int Id { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+        public List<CharacterDirectivesOutlineObjective> Objectives { get; set; }
+    }
+
+    public class CharacterDirectivesOutlineObjective
+    {
+        public int Id { get; set; }
+        public int? Progress { get; set; }
+    }
+}
---
+++ src/Voidwell.DaybreakGames.Domain/Models/DirectivesOutline.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Voidwell.DaybreakGames.Domain.Models
+{
+    public class DirectivesOutline
+    {
+        public List<DirectivesOutlineTreeCategory> Categories { get; set; }
+    }
+
+    public class DirectivesOutlineTreeCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<DirectivesOutlineTree> Trees { get; set; }
+    }
+
+    public class DirectivesOutlineTree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? ImageId { get; set; }
+
+        public List<DirectivesOutlineTier> Tiers { get; set; }
+    }
+
+    public class DirectivesOutlineTier
+    {
+        public int TierId { get; set; }
+        public int TreeId { get; set; }
+        public int? DirectivePoints { get; set; }
+        public int CompletionCount { get; set; }
+        public string Name { get; set; }
+        public int? ImageId { get; set; }
+
+        public List<DirectivesOutlineReward> Rewards { get; set; }
+        public List<DirectivesOutlineDirective> Directives { get; set; }
+    }
+
+    public class DirectivesOutlineReward
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ImageId { get; set; }
+        public int? FactionId { get; set; }
+    }
+
+    public class DirectivesOutlineDirective
+    {
+        public int Id { get; set; }
+        public int TreeId { get; set; }
+        public int TierId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? ImageId { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+        public IEnumerable<DirectivesOutlineObjective> Objectives { get; set; }
+    }
+
+    public class DirectivesOutlineObjective
+    {
+        public int Id { get; set; }
+        public int TypeId { get; set; }
+        public int? GoalValue { get; set; }
+    }
+}
